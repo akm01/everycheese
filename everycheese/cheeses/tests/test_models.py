6e2fb610d9adcdf98b35a0edcc424621d4feb4ae
Alok Mehta <Alok.Mehta@its.ny.gov>
2020-08-23 14:05:16 -0400

    Added Factory to test_models

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -4,13 +4,13 @@ import pytest
 # connects our tests with our database
 pytestmark = pytest.mark.django_db
 
+from .factories import CheeseFactory
+from ..models import Cheese
+
+
 from ..models import Cheese
 def test__str__():
-    cheese = Cheese.objects.create(
-        name="Stracchino",
-        description="Semi-sweet cheese that goes well with starches.",
-        firmness=Cheese.Firmness.SOFT
-    )
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
 
