1a21df74bc19fa2b372f9bf030d74770efe61a54
Alok Mehta <Alok.Mehta@its.ny.gov>
2020-08-23 15:26:19 -0400

    First pass at working Add Cheese Form

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -11,8 +11,15 @@ urlpatterns = [
     ),
 
     path(
+        route='add/',
+        view=views.CheeseCreateView.as_view(),
+        name="add"
+    ),
+
+    path(
         route='<slug:slug>',
         view=views.CheeseDetailView.as_view(),
         name="detail"
     ),
+
 ]
